e1792191dd559d4ef1d43a5f23417c3fe72804fb
Bli_Aik <haikun2333@126.com>
2025-10-22 11:53:36 +0800

    feat(core): add app state management and texture loading system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use bevy::asset::LoadedFolder;
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .init_state::<AppState>()
+        .add_systems(OnEnter(AppState::Setup), load_textures)
+        .run();
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
+enum AppState {
+    #[default]
+    Setup,
+    Finished,
+}
+
+#[derive(Resource, Default)]
+struct OverWorldCharacterSpriteFolder(Handle<LoadedFolder>);
+
+fn load_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(OverWorldCharacterSpriteFolder(asset_server.load_folder("textures/overworld/characters")));
 }
