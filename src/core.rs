71b14fd504b829ac15040694bab54da841c82e06
Bli_Aik <haikun2333@126.com>
2025-10-23 12:56:31 +0800

    feat(core): add input management system and restructure modules

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,6 @@
 pub(crate) mod core_bundles;
 mod core_components;
+pub(crate) mod input;
 pub(crate) mod overworld;
 pub(crate) mod resource;
 pub(crate) mod setup;
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
 mod core;
 mod extra;
 
+use crate::core::input::*;
 use crate::core::overworld::*;
 use crate::core::resource::*;
 use crate::core::setup::*;
+use bevy::app::PluginGroupBuilder;
 use bevy::prelude::*;
 use bevy::window::*;
 use extra::markdown_asset_loader::*;
+use leafwing_input_manager::prelude::*;
+use seldom_state::prelude::*;
 
 fn main() {
-    let resolution_scale = ResolutionScale::default();
+    let default_plugins = get_default_plugins();
 
     App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(ImagePlugin::default_nearest())
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        resolution: WindowResolution::new(
-                            320 * resolution_scale.get(),
-                            240 * resolution_scale.get(),
-                        ),
-                        resizable: false,
-                        ..default()
-                    }),
-                    ..default()
-                }),
-        )
-        .add_plugins(MarkdownPlugin)
+        .add_plugins((
+            default_plugins,
+            MarkdownPlugin,
+            InputManagerPlugin::<Action>::default(),
+            StateMachinePlugin::default(),
+        ))
         .init_resource::<ResolutionScale>()
         .init_state::<AppState>()
         .add_plugins((SetupPlugin, OverworldPlugin))
         .run();
+}
+
+fn get_default_plugins() -> PluginGroupBuilder {
+    let resolution_scale = ResolutionScale::default();
+    DefaultPlugins
+        .set(ImagePlugin::default_nearest())
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(
+                    320 * resolution_scale.get(),
+                    240 * resolution_scale.get(),
+                ),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        })
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
