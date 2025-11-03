1cf249515d0a33a018a9050341e5a52a15302d46
Bli_Aik <haikun2333@126.com>
2025-11-04 07:19:38 +0800

    feat(assets): migrate from LDtk to Tiled tilemap format

---
+++ assets/levels/Ruins.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Ruins" tilewidth="20" tileheight="20" tilecount="240" columns="16">
+ <image source="tiles/ruins.png" width="320" height="300"/>
+</tileset>
