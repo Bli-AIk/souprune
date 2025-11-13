5ae0eba78cf7bef21741602a6c9e4ff0517479d7
Bli_Aik <haikun2333@126.com>
2025-11-13 11:10:42 +0800

    refactor(main): reorganize imports and plugin structure

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,20 @@ mod app_states;
 mod core;
 mod extra;
 
-use crate::core::input::{Action, PlayerInputSettings};
-use crate::core::overworld::*;
-use crate::core::setup::*;
-use crate::core::*;
-use crate::extra::debug::DebugPlugin;
-use crate::extra::toml::TomlPlugin;
-use app_states::AppState;
+use std::default::*;
+
 use bevy::app::PluginGroupBuilder;
 use bevy::prelude::*;
 use bevy::window::*;
-use bevy_ecs_tiled::prelude::*;
-use extra::markdown::*;
-use leafwing_input_manager::prelude::*;
-use seldom_state::prelude::*;
+
+use crate::core::*;
+use app_states::AppState;
 
 /// Get the default Bevy plugins with custom window size and image plugin settings.
 ///
 /// 获取具有自定义窗口大小和图像插件设置的默认 Bevy 插件。
 fn get_bevy_default_plugins() -> PluginGroupBuilder {
-    let resolution_scale = ResolutionScale::default();
+    let resolution_scale = setup::ResolutionScale::default();
     DefaultPlugins
         .set(ImagePlugin::default_nearest())
         .set(WindowPlugin {
@@ -42,10 +36,7 @@ fn get_bevy_default_plugins() -> PluginGroupBuilder {
 /// 获取应用程序中使用的文件导入器插件。
 macro_rules! get_file_importer_plugins {
     () => {
-        (
-            MarkdownPlugin,
-            TomlPlugin,
-        )
+        (extra::markdown::MarkdownPlugin, extra::toml::TomlPlugin)
     };
 }
 
@@ -55,10 +46,9 @@ macro_rules! get_file_importer_plugins {
 macro_rules! get_third_plugins {
     () => {
         (
-            get_file_importer_plugins!(),
-            InputManagerPlugin::<Action>::default(),
-            StateMachinePlugin::default(),
-            TiledPlugin::default(),
+            leafwing_input_manager::prelude::InputManagerPlugin::<crate::core::input::Action>::default(),
+            seldom_state::prelude::StateMachinePlugin::default(),
+            bevy_ecs_tiled::prelude::TiledPlugin::default(),
         )
     };
 }
@@ -68,19 +58,25 @@ macro_rules! get_third_plugins {
 /// 获取特定于游戏的插件。
 macro_rules! get_game_plugins {
     () => {
-        (CorePlugin, SetupPlugin, OverworldPlugin, GlobalPlugin)
+        (
+            CorePlugin,
+            setup::SetupPlugin,
+            overworld::OverworldPlugin,
+            GlobalPlugin,
+        )
     };
 }
 fn main() {
     App::new()
+        .init_resource::<setup::ResolutionScale>()
         .add_plugins((
             get_bevy_default_plugins(),
+            get_file_importer_plugins!(),
             get_third_plugins!(),
             #[cfg(feature = "debug")]
-            DebugPlugin,
+            extra::debug::DebugPlugin,
         ))
-        .init_resource::<ResolutionScale>()
-        .init_resource::<PlayerInputSettings>()
+        .init_resource::<input::PlayerInputSettings>()
         .init_state::<AppState>()
         .add_plugins(get_game_plugins!())
         .run();
